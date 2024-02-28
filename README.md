8c2305274650be41e2c98a973ab9907825e6f3dc
Amr-Azab <20194736@fue.edu.eg>
2024-02-28 02:09:58 +0200

    update readme

--- README.md
+++ README.md
@@ -11,3 +11,25 @@ Unit tests
 
 Sqlite
 ![Alt text](img/DsqliteB.PNG)
+
+## Run
+
+you can fill the data in sqlite , can retrive the data , update and delete the data
+
+run the code (run.py)
+
+select the unit test you want (get , delete , post , put) from (tests/test_api.py)
+
+```bash
+pytest tests/test_api.py
+```
+
+go to another git bash to show the data (sqlite)
+
+```bash
+sqlite3 test_cases.db
+```
+
+then write SELECT \* FROM test_cases;
+
+you will see the all the data (id , name , description , execution result)
--- tests/test_api.py
+++ tests/test_api.py
@@ -4,17 +4,19 @@ import requests
 base_url = 'http://localhost:5000'
 
 def test_create_test_case():
-    response = requests.post(f'{base_url}/test_cases', json={'name': 'Test Case 1', 'description': 'Description 1'})
+    # create test (name , description)
+    response = requests.post(f'{base_url}/test_cases', json={'name': 'Advertisements', 'description': 'Advertisements about project'})
     assert response.status_code == 200
     assert response.json()['message'] == 'Test case created successfully'
 
 def test_get_all_test_cases():
+    # get all test 
     response = requests.get(f'{base_url}/test_cases')
     assert response.status_code == 200
     
 
 def test_get_single_test_case():
-    # Assuming there is no test case with ID 1 in the database
+    # get single test by id 
     response = requests.get(f'{base_url}/test_cases/1')
     if response.status_code == 404:
         assert 'Test case not found' in response.json()['message']
@@ -22,25 +24,25 @@ def test_get_single_test_case():
 
 
 def test_update_test_case():
-    # Assuming there is no test case with ID 1 in the database
+    # update the test (name , description) by id
     response = requests.put(f'{base_url}/test_cases/1', json={'name': 'Updated Test Case', 'description': 'Updated Description'})
     if response.status_code == 404:
         assert 'Test case not found' in response.json()['message']
 
 def test_delete_test_case():
-    # Assuming there is no test case with ID 1 in the database
+    # delete the test by id
     response = requests.delete(f'{base_url}/test_cases/1')
     if response.status_code == 404:
         assert 'Test case not found' in response.json()['message']
 
 def test_record_execution_result():
-    # Assuming there is no test case with ID 1 in the database
-    response = requests.post(f'{base_url}/execution_results/1', json={'execution_result': 'Fail'})
+    # record execution result the test by id
+    response = requests.post(f'{base_url}/execution_results/9', json={'execution_result': 'passed'})
     if response.status_code == 404:
         assert 'Test case not found' in response.json()['message']
 
 def test_get_one_execution_result():
-    # Assuming there is no test case with ID 1 in the database
+    # get one execution result the test by id
     response = requests.get(f'{base_url}/execution_results/1')
     if response.status_code == 404:
         assert 'Test case not found' in response.json()['message']
@@ -51,3 +53,10 @@ def test_get_execution_results():
     assert response.status_code == 200
     assert isinstance(response.json(), list)  # Assuming the response should be a list
     # Additional assertions based on the structure of each item in the list
+
+# you can fill the data in sqlite , can retrive the data , update and delete the data 
+# run the code (run.py)    
+# select the unit test you want (get , delete , post , put) and write  pytest tests/test_api.py  in git bash 
+# write sqlite3 test_cases.db  in git bash 
+# and write SELECT * FROM test_cases;  
+# you will see the all the data (id , name , description , execution result)    
